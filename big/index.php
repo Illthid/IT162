43a93f7bfe58576ecd219931daeef677ce61876c
illthid <barnettedavid@gmail.com>
2021-10-25 11:28:24 -0700

    First Commit

---
+++ big/index.php
@@ -0,0 +1,5 @@
+<?php include "includes/header.php";?>
+
+<h2>Big Page</h2>
+
+<?php include "includes/footer.php";?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include "includes/header.php";?>
+
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+
+
+ <p>Hello, My name is David. I am working on my degree in database development.
+    I have been in and out of computers since I was young.Over the last 20 years
+    I've been building my own computers. I am 37 years old. One thing about me
+    is I'm a nerd. A common saying of mine is ‘If it's nerdy I have probably
+    done it’. I play tabletop role playing games. I’ve been to lots of
+    conventions. I also ride motorcycles and go to the gym 3 to 4 days a week.
+    I play lots of video games, mostly open world rpgs but love a good strategy
+    when I can find one.I was in the automotive industry for many years.
+    I can fix most things on a car, I've a love for 1950-1976 Volkswagens.
+    I also do my own work on my motorcycles. I am always looking for new people
+    to hang out with. My roommates and I have 2 wonderful cats, Raistlin and
+    Caraman. We call them race and car… haha.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <!-- <h3>Right Column</h3> -->
+ <!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/tablet.jpg" class="tablet" alt="" />
+ <img src="images/desktop.jpg" class="desktop" alt="" />
+ <img src="images/phone.jpg" class="phone" alt="" />
+</aside>
+<!-- END RIGHT COL -->
+
+<!-- </main> -->
+<!-- END WRAPPER -->
+
+<?php include "includes/footer.php";?>
